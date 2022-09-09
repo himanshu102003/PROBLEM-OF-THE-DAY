0fae186d5ffb9fabedcd13238f1ac6a3ec189227
himanshu102003 <109969182+himanshu102003@users.noreply.github.com>
2022-09-09 06:52:17 +0530

    Add files via upload

---
+++ fill_the_grid.py
@@ -0,0 +1,18 @@
+# Problem of the day FILL THE GRID
+
+def tiles(count,n,m):
+    if n%2==0 and m%2==0:
+        return 0
+    elif n%2==0:
+        return n 
+    elif m%2==0:
+        return m
+    else:
+        return (n+m-1)
+        
+if __name__ == '__main__':
+    t=int(input())
+    for i in range(t):
+        x,y=map(int,input().split())
+        
+        print (tiles(0,x,y))
